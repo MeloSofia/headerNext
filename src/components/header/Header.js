c762fe5419d65427d8ed11ceb89551c7b305ae8a
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-11-22 07:39:26 -0400

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,26 @@
-import Link from "next/link"
-import Image from "next/image"
-import styles from "../../style/header.module.css"
-import {Poppins} from "next/font/google"
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../style/header.module.css";
 
-export default function Header(){
-    return(
-        <nav>
-            <div>
-                <Image src="/assets/s.png" width="30" height="30" alt="sofia"/>
-                <h1>Sofia Melo</h1>
-            </div>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/About">Sobre</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+
+export default function Header() {
+	return (
+		<nav>
+			<div>
+				<Image src="/assets/s.png" width="30" height="30" alt="sofia" />
+				<h1>Sofia Melo</h1>
+			</div>
+			<ul>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/About">Sobre</Link>
+				</li>
+				<li>
+					<Link href="/Contact">Contato</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
