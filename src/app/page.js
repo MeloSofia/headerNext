951f2d785d53d527875f45fe859f32bc47d1bcb6
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-11-27 01:59:00 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "../style/page.module.css";
 export default function Home() {
 	return (
 		<div>
-			<p className={styles.p}>Início</p>
+			<p className={styles.p}>Início!</p>
 		</div>
 	);
 }
